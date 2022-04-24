9f774a91237e57ad5a5d8591f27aa14756627235
Zhupf <zhupff@qq.com>
2022-04-24 18:42:02 +0800

    ~

--- module-home/module-home-core/src/main/java/the/gadget/modulehome/ModuleHomeApiImpl.kt
+++ module-home/module-home-core/src/main/java/the/gadget/modulehome/ModuleHomeApiImpl.kt
@@ -3,6 +3,7 @@ package the.gadget.modulehome
 import android.content.Context
 import android.content.Intent
 import com.google.auto.service.AutoService
+import the.gadget.modulehome.activity.HomeActivity
 
 @AutoService(ModuleHomeApi::class)
 class ModuleHomeApiImpl : ModuleHomeApi {
--- module-home/module-home-core/src/main/java/the/gadget/modulehome/activity/HomeActivity.kt
+++ module-home/module-home-core/src/main/java/the/gadget/modulehome/activity/HomeActivity.kt
@@ -1,9 +1,16 @@
-package the.gadget.modulehome
+package the.gadget.modulehome.activity
 
+import android.os.Bundle
 import the.gadget.modulebase.activity.BindingActivity
 import the.gadget.modulehomecore.databinding.HomeActivityBinding
 import the.gadget.modulehomecore.R
 
 class HomeActivity : BindingActivity<HomeActivityBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding
+    }
+
     override fun getLayoutRes(): Int = R.layout.home_activity
 }
