c363eaadb360c6f09c2d16eca46f62e07769b0fb
Zhupff <zhupff@qq.com>
2026-02-11 03:08:42 +0800

    ~

--- basic/basic-theme/src/plus/kotlin/gadget/basic/theme/LightTheme.kt
+++ basic/basic-theme/src/plus/kotlin/gadget/basic/theme/LightTheme.kt
@@ -1,6 +1,12 @@
 package gadget.basic.theme
 
-internal abstract class LightTheme(
+import com.google.mcu.hct.Hct
+import com.google.mcu.scheme.SchemeTonalSpot
+import gadget.basic.Gadget
+import gadget.basic.exception.throws
+import gadget.basic.log.logW
+
+internal open class LightTheme(
     name: String,
     parent: LightTheme? = null,
 ) : AbstractTheme<LightTheme>(name, parent) {
@@ -9,12 +15,59 @@ internal abstract class LightTheme(
 
     final override val id: String = "${name}-Light"
 
-    override val primaryColor   : Int = 0xFF445E91.toInt()
-    override val onPrimaryColor : Int = 0xFFFFFFFF.toInt()
-    override val backgroundColor: Int = 0xFFFFFFFF.toInt()
-    override val foregroundColor: Int = 0xFF1A1B20.toInt()
-    override val surfaceColor   : Int = 0xFFEDEDF4.toInt()
-    override val outlineColor   : Int = 0xFF74777F.toInt()
-    override val errorColor     : Int = 0xFFBA1A1A.toInt()
-    override val onErrorColor   : Int = 0xFFFFFFFF.toInt()
+    override var primaryColor   : Int = 0xFF445E91.toInt()
+        protected set
+    override var onPrimaryColor : Int = 0xFFFFFFFF.toInt()
+        protected set
+    override var backgroundColor: Int = 0xFFFFFFFF.toInt()
+        protected set
+    override var foregroundColor: Int = 0xFF1A1B20.toInt()
+        protected set
+    override var surfaceColor   : Int = 0xFFEDEDF4.toInt()
+        protected set
+    override var outlineColor   : Int = 0xFF74777F.toInt()
+        protected set
+    override var errorColor     : Int = 0xFFBA1A1A.toInt()
+        protected set
+    override var onErrorColor   : Int = 0xFFFFFFFF.toInt()
+        protected set
+
+    var night: NightTheme? = null
+        protected set
+
+    fun create(name: String, argb: Int, night: NightTheme?): LightTheme {
+        if (name == this.name) {
+            if (Gadget.debuggable) {
+                IllegalArgumentException("The theme with the same name is already exists!").throws()
+            } else {
+                logW(null) { "The theme with the same name is already exists!" }
+                return LightTheme
+            }
+        }
+        if (argb == 0) {
+            if (Gadget.debuggable) {
+                IllegalArgumentException("argb should not be 0!").throws()
+            } else {
+                logW(null) { "argb should not be 0!" }
+                return LightTheme
+            }
+        }
+        val scheme = SchemeTonalSpot(Hct.fromInt(argb), false, 0.0)
+        val theme = LightTheme(name, this).also { theme ->
+            theme.primaryColor = scheme.primary
+            theme.onPrimaryColor = scheme.onPrimary
+            theme.backgroundColor = scheme.surfaceContainerLowest
+            theme.foregroundColor = scheme.onSurface
+            theme.surfaceColor = scheme.surfaceContainer
+            theme.outlineColor = scheme.outline
+            theme.errorColor = scheme.error
+            theme.onErrorColor = scheme.onError
+        }
+        if (night != null) {
+            theme.night = night
+        } else {
+            theme.night = this.night?.create(name, argb, theme)
+        }
+        return theme
+    }
 }
--- basic/basic-theme/src/plus/kotlin/gadget/basic/theme/NightTheme.kt
+++ basic/basic-theme/src/plus/kotlin/gadget/basic/theme/NightTheme.kt
@@ -1,6 +1,12 @@
 package gadget.basic.theme
 
-internal abstract class NightTheme(
+import com.google.mcu.hct.Hct
+import com.google.mcu.scheme.SchemeTonalSpot
+import gadget.basic.Gadget
+import gadget.basic.exception.throws
+import gadget.basic.log.logW
+
+internal open class NightTheme(
     name: String,
     parent: NightTheme? = null,
 ) : AbstractTheme<NightTheme>(name, parent) {
@@ -9,12 +15,59 @@ internal abstract class NightTheme(
 
     final override val id: String = "${name}-Night"
 
-    override val primaryColor   : Int = 0xFFADC6FF.toInt()
-    override val onPrimaryColor : Int = 0xFF102F60.toInt()
-    override val backgroundColor: Int = 0xFF0C0E13.toInt()
-    override val foregroundColor: Int = 0xFFE2E2E9.toInt()
-    override val surfaceColor   : Int = 0xFF1E1F25.toInt()
-    override val outlineColor   : Int = 0xFF8E9099.toInt()
-    override val errorColor     : Int = 0xFFFFB4AB.toInt()
-    override val onErrorColor   : Int = 0xFF690005.toInt()
+    override var primaryColor   : Int = 0xFFADC6FF.toInt()
+        protected set
+    override var onPrimaryColor : Int = 0xFF102F60.toInt()
+        protected set
+    override var backgroundColor: Int = 0xFF0C0E13.toInt()
+        protected set
+    override var foregroundColor: Int = 0xFFE2E2E9.toInt()
+        protected set
+    override var surfaceColor   : Int = 0xFF1E1F25.toInt()
+        protected set
+    override var outlineColor   : Int = 0xFF8E9099.toInt()
+        protected set
+    override var errorColor     : Int = 0xFFFFB4AB.toInt()
+        protected set
+    override var onErrorColor   : Int = 0xFF690005.toInt()
+        protected set
+
+    var light: LightTheme? = null
+        protected set
+
+    fun create(name: String, argb: Int, light: LightTheme?): NightTheme {
+        if (name == this.name) {
+            if (Gadget.debuggable) {
+                IllegalArgumentException("The theme with the same name is already exists!").throws()
+            } else {
+                logW(null) { "The theme with the same name is already exists!" }
+                return NightTheme
+            }
+        }
+        if (argb == 0) {
+            if (Gadget.debuggable) {
+                IllegalArgumentException("argb should not be 0!").throws()
+            } else {
+                logW(null) { "argb should not be 0!" }
+                return NightTheme
+            }
+        }
+        val scheme = SchemeTonalSpot(Hct.fromInt(argb), true, 0.0)
+        val theme = NightTheme(name, this).also { theme ->
+            theme.primaryColor = scheme.primary
+            theme.onPrimaryColor = scheme.onPrimary
+            theme.backgroundColor = scheme.surfaceContainerLowest
+            theme.foregroundColor = scheme.onSurface
+            theme.surfaceColor = scheme.surfaceContainer
+            theme.outlineColor = scheme.outline
+            theme.errorColor = scheme.error
+            theme.onErrorColor = scheme.onError
+        }
+        if (light != null) {
+            theme.light = light
+        } else {
+            theme.light = this.light?.create(name, argb, theme)
+        }
+        return theme
+    }
 }
