ffcca85eeb5c372ee8170606261994ce084c5fa1
Zhupff <zhupff@qq.com>
2026-02-11 03:29:32 +0800

    ~

--- basic/basic-theme/src/plus/kotlin/gadget/basic/theme/ThemeManager.kt
+++ basic/basic-theme/src/plus/kotlin/gadget/basic/theme/ThemeManager.kt
@@ -7,10 +7,16 @@ import gadget.basic.Gadget
 
 object ThemeManager : AbstractThemeManager(), Observer<Gadget.AppLifecycle.State> {
 
+    @Volatile var isLightMode: Boolean = !Gadget.application.resources.configuration.isNightModeActive
+        private set(value) {
+            if (field != value) {
+                field = value
+            }
+        }
+
     private val observable = MutableLiveData<Theme>()
 
     init {
-        val isLightMode = !Gadget.application.resources.configuration.isNightModeActive
         if (isLightMode) {
             observable.postValue(LightTheme)
         } else {
@@ -22,7 +28,7 @@ object ThemeManager : AbstractThemeManager(), Observer<Gadget.AppLifecycle.State
 
     override fun onChanged(value: Gadget.AppLifecycle.State) {
         if (value is Gadget.AppLifecycle.State.OnAppConfigurationChanged) {
-            val isLightMode = !value.newConfiguration.isNightModeActive
+            isLightMode = !value.newConfiguration.isNightModeActive
             val currentTheme = observable.value
             if (isLightMode && currentTheme is NightTheme) {
                 currentTheme.light?.let(observable::postValue)
@@ -30,5 +36,30 @@ object ThemeManager : AbstractThemeManager(), Observer<Gadget.AppLifecycle.State
                 currentTheme.night?.let(observable::postValue)
             }
         }
+    }
+
+    fun switch(newTheme: Theme) {
+        val currentTheme = observable.value
+        if (newTheme.id == currentTheme?.id) {
+            return
+        }
+        if (isLightMode) {
+            if (newTheme is LightTheme) {
+                observable.postValue(newTheme)
+                return
+            } else if (newTheme is NightTheme) {
+                observable.postValue(newTheme.light ?: newTheme)
+                return
+            }
+        } else {
+            if (newTheme is NightTheme) {
+                observable.postValue(newTheme)
+                return
+            } else if (newTheme is LightTheme) {
+                observable.postValue(newTheme.night ?: newTheme)
+                return
+            }
+        }
+        observable.postValue(newTheme)
     }
 }
