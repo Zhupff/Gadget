74fe5a27f4ec730d7d6d40a94d382c9a9a1da610
Zhupf <zhupff@qq.com>
2026-02-14 14:38:48 +0800

    ~

---
+++ basic/src/main/kotlin/gadget/basic/arch/GadgetActivity.kt
@@ -0,0 +1,95 @@
+package gadget.basic.arch
+
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import gadget.basic.log.Loggable
+import gadget.basic.log.logD
+import gadget.basic.log.logI
+
+open class GadgetActivity : AppCompatActivity(), Loggable by Loggable.Tag() {
+
+    protected open lateinit var windowInsetsControllerCompat: WindowInsetsControllerCompat
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        logD { "onAttachedToWindow()" }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        logI { "onCreate($savedInstanceState)" }
+
+        windowInsetsControllerCompat = WindowInsetsControllerCompat(window, window.decorView)
+        windowInsetsControllerCompat.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        if (resources.configuration.orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            windowInsetsControllerCompat.show(WindowInsetsCompat.Type.systemBars())
+        } else {
+            windowInsetsControllerCompat.hide(WindowInsetsCompat.Type.systemBars())
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logD { "onRestart()" }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logI { "onStart()" }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        logD { "onRestoreInstanceState($savedInstanceState)" }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logI { "onResume()" }
+    }
+
+    override fun onTopResumedActivityChanged(isTopResumedActivity: Boolean) {
+        super.onTopResumedActivityChanged(isTopResumedActivity)
+        logD { "onTopResumedActivityChanged($isTopResumedActivity)" }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logI { "onPause()" }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        logD { "onSaveInstanceState(${outState})" }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logI { "onStop()" }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logI { "onDestroy()" }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        logD { "onDetachedFromWindow()" }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        logD { "onConfigurationChanged($newConfig)" }
+
+        if (newConfig.orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            windowInsetsControllerCompat.show(WindowInsetsCompat.Type.systemBars())
+        } else {
+            windowInsetsControllerCompat.hide(WindowInsetsCompat.Type.systemBars())
+        }
+    }
+}
