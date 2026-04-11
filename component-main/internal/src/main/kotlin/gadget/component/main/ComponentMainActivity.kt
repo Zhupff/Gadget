8a40c1111cf95844fa1967f671463295b2856a76
Zhupf <zhupff@qq.com>
2026-04-12 02:11:33 +0800

    ~

--- component-main/internal/src/main/kotlin/gadget/component/main/ComponentMainActivity.kt
+++ component-main/internal/src/main/kotlin/gadget/component/main/ComponentMainActivity.kt
@@ -1,17 +1,11 @@
 package gadget.component.main
 
 import android.os.Bundle
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.lifecycleScope
-import androidx.window.layout.FoldingFeature
-import androidx.window.layout.WindowInfoTracker
-import androidx.window.layout.WindowLayoutInfo
 import gadget.basic.arch.GadgetActivity
 import gadget.component.main.internal.databinding.ComponentMainActivityBinding
 import gadget.component.main.layout.ComponentMainLayout
-import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class ComponentMainActivity : GadgetActivity() {
@@ -25,39 +19,13 @@ class ComponentMainActivity : GadgetActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        splashScreen.setOnExitAnimationListener { splash ->
+            lifecycleScope.launch {
+                delay(500L)
+                splash.remove()
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(ComponentMainLayout(this))
-
-//        lifecycleScope.launch {
-//            WindowInfoTracker.getOrCreate(this@ComponentMainActivity)
-//                .windowLayoutInfo(this@ComponentMainActivity)
-//                .collect(object : FlowCollector<WindowLayoutInfo> {
-//                    private var once = false
-//                    private var fold = false
-//                    override suspend fun emit(value: WindowLayoutInfo) {
-//                        val flat = value.displayFeatures.filterIsInstance<FoldingFeature>().firstOrNull()
-//                            ?.let { it.state == FoldingFeature.State.FLAT || it.state == FoldingFeature.State.HALF_OPENED }
-//                            ?: false
-//                        if (!once || fold == flat) {
-//                            once = true
-//                            fold = !flat
-//                            onWindowChanged(fold)
-//                        }
-//                    }
-//                })
-//        }
     }
-
-//    private fun onWindowChanged(folding: Boolean) {
-//        val drawerFragment = supportFragmentManager.findFragmentByTag(DrawerFragment::class.java.simpleName) as? DrawerFragment ?: DrawerFragment()
-//        if (folding) {
-//            drawerFragment.attachTo(this, viewBinding.mainDrawer)
-//            viewBinding.mainContainer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, viewBinding.mainDrawer)
-//            viewBinding.sideContainer.isGone = true
-//        } else {
-//            drawerFragment.attachTo(this, viewBinding.sideDrawer)
-//            viewBinding.mainContainer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, viewBinding.mainDrawer)
-//            viewBinding.sideContainer.isVisible = true
-//        }
-//    }
 }
