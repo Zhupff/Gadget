0eaa592747bca259f948602ce290c7dc5d697e26
Zhupf <zhupff@qq.com>
2026-04-12 14:27:57 +0800

    ~

--- basic/ui/src/main/kotlin/gadget/basic/ui/view/FrameLayoutX.kt
+++ basic/ui/src/main/kotlin/gadget/basic/ui/view/FrameLayoutX.kt
@@ -1,19 +1,27 @@
 package gadget.basic.ui.view
 
 import android.content.Context
+import android.graphics.Outline
+import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import android.view.WindowInsets
 import android.widget.FrameLayout
+import androidx.annotation.UiThread
 import androidx.core.view.WindowInsetsCompat
+import gadget.basic.Gadget
 import gadget.basic.annotation.DslScope
+import gadget.basic.exception.throws
 import gadget.basic.ui.common.GravityX
 import gadget.basic.ui.dsl.ViewScope
 import gadget.basic.ui.dsl.marginLayoutParams
+import kotlin.math.min
 
 @DslScope
 class FrameLayoutX(context: Context) : FrameLayout(context) {
 
-    var fitWindowInsetGravity: Int = GravityX.A
+    var fitWindowInsetGravity: Int = GravityX.N
+        @UiThread
         set(value) {
             var target = GravityX.N
             if (value and GravityX.A == GravityX.A) {
@@ -38,6 +46,121 @@ class FrameLayoutX(context: Context) : FrameLayout(context) {
             }
         }
 
+    var clipCornerGravity: Int = GravityX.N
+        @UiThread
+        set(value) {
+            var target = GravityX.N
+            if (value and GravityX.A == GravityX.A) {
+                target = GravityX.A
+            } else if (value and GravityX.L == GravityX.L) {
+                target = GravityX.L
+            } else if (value and GravityX.T == GravityX.T) {
+                target = GravityX.T
+            } else if (value and GravityX.R == GravityX.R) {
+                target = GravityX.R
+            } else if (value and GravityX.B == GravityX.B) {
+                target = GravityX.B
+            } else if (value and GravityX.TL == GravityX.TL) {
+                target = GravityX.TL
+            } else if (value and GravityX.TR == GravityX.TR) {
+                target = GravityX.TR
+            } else if (value and GravityX.BR == GravityX.BR) {
+                target = GravityX.BR
+            } else if (value and GravityX.BL == GravityX.BL) {
+                target = GravityX.BL
+            } else {
+                target = GravityX.N
+            }
+            if (target != value && Gadget.debuggable) {
+                IllegalArgumentException("Illegal clipCornerGravity value: $value!").throws()
+            }
+            if (field != target) {
+                field = target
+                invalidateOutline()
+            }
+        }
+
+    var clipCornerRadius: Float = 0F
+        @UiThread
+        set(value) {
+            val target = if (value < 0F) -1F else value
+            if (field != target) {
+                field = target
+                invalidateOutline()
+            }
+        }
+
+    private val clipCornerRadiusArray = FloatArray(8)
+
+    init {
+        clipToOutline = true
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                val diameter = min(width, height)
+                val r = if (clipCornerRadius < 0F) diameter / 2F else min(clipCornerRadius, diameter / 2F)
+                clipCornerRadiusArray.fill(0F)
+                println("@@@ ${clipCornerGravity} ${clipCornerRadius} ${r}")
+                when (clipCornerGravity) {
+                    GravityX.N -> {
+                        outline.setRect(0, 0, width, height)
+                    }
+                    GravityX.L -> {
+                        clipCornerRadiusArray[0] = r
+                        clipCornerRadiusArray[1] = r
+                        clipCornerRadiusArray[6] = r
+                        clipCornerRadiusArray[7] = r
+                        outline.setRoundRect(0, 0, width + r.toInt(), height, r)
+                    }
+                    GravityX.T -> {
+                        clipCornerRadiusArray[0] = r
+                        clipCornerRadiusArray[1] = r
+                        clipCornerRadiusArray[2] = r
+                        clipCornerRadiusArray[3] = r
+                        outline.setRoundRect(0, 0, width, height + r.toInt(), r)
+                    }
+                    GravityX.R -> {
+                        clipCornerRadiusArray[2] = r
+                        clipCornerRadiusArray[3] = r
+                        clipCornerRadiusArray[4] = r
+                        clipCornerRadiusArray[5] = r
+                        outline.setRoundRect(0 - r.toInt(), 0, width, height, r)
+                    }
+                    GravityX.B -> {
+                        clipCornerRadiusArray[4] = r
+                        clipCornerRadiusArray[5] = r
+                        clipCornerRadiusArray[6] = r
+                        clipCornerRadiusArray[7] = r
+                        outline.setRoundRect(0, 0 - r.toInt(), width, height, r)
+                    }
+                    GravityX.TL -> {
+                        clipCornerRadiusArray[0] = r
+                        clipCornerRadiusArray[1] = r
+                        outline.setRoundRect(0, 0, width + r.toInt(), height + r.toInt(), r)
+                    }
+                    GravityX.TR -> {
+                        clipCornerRadiusArray[2] = r
+                        clipCornerRadiusArray[3] = r
+                        outline.setRoundRect(0 - r.toInt(), 0, width, height + r.toInt(), r)
+                    }
+                    GravityX.BR -> {
+                        clipCornerRadiusArray[4] = r
+                        clipCornerRadiusArray[5] = r
+                        outline.setRoundRect(0 - r.toInt(), 0 - r.toInt(), width, height, r)
+                    }
+                    GravityX.BL -> {
+                        clipCornerRadiusArray[6] = r
+                        clipCornerRadiusArray[7] = r
+                        outline.setRoundRect(0, 0 - r.toInt(), width + r.toInt(), height, r)
+                    }
+                    GravityX.A -> {
+                        clipCornerRadiusArray.fill(r)
+                        outline.setRoundRect(0, 0, width, height, r)
+                    }
+                }
+            }
+        }
+    }
+
     override fun onApplyWindowInsets(insets: WindowInsets?): WindowInsets? {
         return super.onApplyWindowInsets(insets).also {
             applyWindowInsets()
@@ -60,6 +183,17 @@ class FrameLayoutX(context: Context) : FrameLayout(context) {
             paddingBottom != targetPaddingBottom) {
             setPadding(targetPaddingLeft, targetPaddingTop, targetPaddingRight, targetPaddingBottom)
         }
+    }
+
+    override fun setClipToOutline(clipToOutline: Boolean) {
+        if (!clipToOutline) {
+            if (Gadget.debuggable) {
+                IllegalArgumentException("Must support clipToOutline!").throws()
+            } else {
+                return
+            }
+        }
+        super.setClipToOutline(true)
     }
 
     override fun onAttachedToWindow() {
