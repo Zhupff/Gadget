3c57e5c56cff7b62c60b25e0ae472b209e88f7ab
Zhupff <zhupff@qq.com>
2026-07-18 17:21:13 +0800

    ~

--- client/src/main/kotlin/gadget/MainActivity.kt
+++ client/src/main/kotlin/gadget/MainActivity.kt
@@ -1,5 +1,6 @@
 package gadget
 
+import alyx.gadget.R
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import gadget.basic.activity.GadgetActivity
@@ -16,5 +17,6 @@ class MainActivity : GadgetActivity() {
             }
         }
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
     }
 }
