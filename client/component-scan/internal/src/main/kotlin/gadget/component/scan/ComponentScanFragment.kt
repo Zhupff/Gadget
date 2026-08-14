6c9ce9ff9f1e726966522151bf5cec235a0db3f1
Zhupf <zhupff@qq.com>
2026-08-14 20:20:23 +0800

    ~

---
+++ client/component-scan/internal/src/main/kotlin/gadget/component/scan/ComponentScanFragment.kt
@@ -0,0 +1,6 @@
+package gadget.component.scan
+
+import gadget.basic.fragment.GadgetFragment
+
+class ComponentScanFragment : GadgetFragment() {
+}
--- client/src/main/kotlin/gadget/ScanActivity.kt
+++ client/src/main/kotlin/gadget/ScanActivity.kt
@@ -1,6 +1,13 @@
 package gadget
 
+import alyx.gadget.R
+import android.os.Bundle
 import gadget.basic.activity.GadgetActivity
 
 class ScanActivity : GadgetActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scan_activity)
+    }
 }
