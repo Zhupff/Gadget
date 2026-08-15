470294a69a64cd6f8fbab8e84a69702f0bf86866
Zhupf <zhupff@qq.com>
2026-08-15 14:48:38 +0800

    ~

--- common/src/main/kotlin/gadget/basic/link/GLink.kt
+++ common/src/main/kotlin/gadget/basic/link/GLink.kt
@@ -3,13 +3,15 @@ package gadget.basic.link
 import java.net.URLDecoder
 
 /**
- * gadget://<biz>[:<ver>]?k1=v1&k2=v2
+ * gadget://<biz>[:<ver>][/<path>...]?k1=v1&k2=v2
  */
 data class GLink(
     /** 所属业务 */
     val biz: String,
     /** 版本 */
     val ver: Int,
+    /** 业务路径 */
+    val path: List<String>,
     /** 参数 */
     val params: Map<String, String>
 ) {
@@ -20,7 +22,8 @@ data class GLink(
             if (!link.startsWith(SCHEME)) return null
 
             val content = link.removePrefix(SCHEME).split('?', limit = 2)
-            val header = content.first().split(':', limit = 2)
+            val route = content.first().split('/')
+            val header = route.first().split(':', limit = 2)
 
             val biz = header[0].takeIf { it.isNotBlank() } ?: return null
             val rawVer = header.getOrNull(1)
@@ -28,6 +31,10 @@ data class GLink(
                 0
             } else {
                 rawVer.toIntOrNull()?.takeIf { it >= 0 } ?: return null
+            }
+            val path = route.drop(1).map { segment ->
+                if (segment.isEmpty()) return null
+                decode(segment) ?: return null
             }
             val params = content.getOrNull(1)
                 ?.takeIf { it.isNotEmpty() }
@@ -41,7 +48,7 @@ data class GLink(
                 }
                 .orEmpty()
 
-            return GLink(biz, ver, params)
+            return GLink(biz, ver, path, params)
         }
 
         private fun decode(value: String): String? = try {
